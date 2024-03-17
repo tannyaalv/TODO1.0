6950245f7ea8637f4de038324802bcb6a4cb8012
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-03-17 00:36:26 -0500

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Alvarez-Salsedo, Tanya on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
